8479dbbf61100fd60c650b60374dc8a7a26badea
Hassan Ezz <dhassanezz98@gmail.com>
2023-05-21 18:47:35 +0300

    update: now using BFS instead of backtracking

---
+++ src/graphTesting.ts
@@ -0,0 +1,98 @@
+import { SIZE } from './constants'
+import { make2dArray } from './makeArray'
+
+export class Pair {
+  first: number
+  second: number
+
+  constructor(first: number, second: number) {
+    this.first = first
+    this.second = second
+  }
+}
+
+async function sleep(time = 10) {
+  await new Promise((r) => setTimeout(r, time))
+}
+
+
+export async function getShortestPath(
+  start: Pair,
+  end: Pair,
+  blocks: boolean[][]
+) {
+  const dx = [-1, 1, 0, 0]
+  const dy = [0, 0, 1, -1]
+
+  const isSafe = (x: number, y: number) =>
+    x < SIZE && y < SIZE && x >= 0 && y >= 0 && !blocks[x][y]
+
+  const setBoxColor = (i: number, j: number, color = 'yellow') => {
+    if((start.first != i || start.second != j) && (end.first != i || end.second != j))
+    document.getElementById(`c-${i}-${j}`)!.style.backgroundColor = color
+  }
+
+  async function bfs(startNode: Pair, endNode: Pair) {
+    const visited: boolean[][] = make2dArray(SIZE, false)
+    const distance: number[][] = make2dArray(SIZE, Number.MAX_SAFE_INTEGER)
+    const parent = make2dArray(
+      SIZE,
+      new Pair(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+    )
+
+    let isSolvable = false
+
+    parent[startNode.first][startNode.second] = new Pair(-1, -1)
+
+    const q: Pair[] = []
+    q.push(startNode)
+
+    while (q.length > 0) {
+      await sleep()
+
+      const p = q[0]
+      visited[p.first][p.second] = true
+      q.shift()
+
+      setBoxColor(p.first, p.second)
+
+      if (p.first === endNode.first && p.second === endNode.second) {
+        isSolvable = true
+        break
+      }
+
+      for (let i = 0; i < 4; i++) {
+        const x = p.first + dx[i],
+          y = p.second + dy[i]
+
+        if (isSafe(x, y) && !visited[x][y]) {
+          distance[x][y] = distance[p.first][p.second] + 1
+          visited[x][y] = true
+          parent[x][y] = p
+
+          q.push(new Pair(x, y))
+        }
+      }
+    }
+
+    if (isSolvable) {
+      const dq: Pair[] = []
+      let p1 = endNode.first,
+        p2 = endNode.second
+
+      while (p1 != -1 && p1 != Number.MIN_SAFE_INTEGER) {
+        dq.unshift(new Pair(p1, p2))
+
+        const tempPair = parent[p1][p2]
+        p1 = tempPair.first
+        p2 = tempPair.second
+      }
+
+      return dq
+    }
+
+    return null
+  }
+
+  return await bfs(start, end)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,123 +1,99 @@
+import { SIZE } from './constants'
+import { Pair, getShortestPath } from './graphTesting'
 import { make2dArray } from './makeArray'
 import './style.css'
 
-const SIZE = 5
-let mat: number[][] = make2dArray(SIZE, 1), visited = make2dArray(SIZE, false)
+let mat: number[][] = make2dArray(SIZE, 1)
 
-let v: [number, number][] = []
-const sols: [number, number][][] = []
+const blocks = make2dArray(SIZE, false)
 
 function main() {
-  const grid = document.getElementById('grid'), select = document.querySelector("#mode"), startBtn = document.querySelector('#start')
+  const grid = document.getElementById('grid'),
+    select = document.querySelector('#mode'),
+    startBtn = document.querySelector('#start')
   let mode: 'blocks' | 'target' | 'location' = 'blocks'
 
   grid!.style.gridTemplateColumns = `repeat(${SIZE}, 1fr)`
-  
 
-  let target = [SIZE-1, SIZE-1], location = [0 , 0]
+  let target = new Pair(SIZE - 1, SIZE - 1),
+    location = new Pair(0, 0)
 
   function render() {
-    grid!.innerHTML = ""
+    grid!.innerHTML = ''
     mat.forEach((arr, i) =>
-      arr.forEach((num, j) => {
-        grid!.innerHTML += `<button id="c-${i}-${j}" i="${i}" j="${j}" class="aspect-square border border-black box ${
-          num === 0 ? 'bg-black text-white' : (location[0] === i && location[1] === j ? 'bg-green-500' : (target[0] === i && target[1] == j && "bg-blue-500"))
-        }">${i},${j}</button>`
+      arr.forEach((_, j) => {
+        const isLocation = location.first === i && location.second === j
+        const isTarget = target.first === i && target.second === j
+        grid!.innerHTML += `<button id="c-${i}-${j}" i="${i}" j="${j}" class="box aspect-square border border-black ${
+          blocks[i][j] ? 'bg-black' : (isLocation || isTarget) && 'bg-green-600'
+        }"></button>`
       })
     )
 
     document.querySelectorAll('.box').forEach((box) => {
       box.addEventListener('click', () => {
-        const [i, j] = box.innerHTML.split(',').map(i => +i)
-        console.log(mode)
+        const i = +box.getAttribute('i')!,
+          j = +box.getAttribute('j')!
+
+        console.log('Clicked')
+
         if (mode === 'blocks') {
-          mat[i][j] = mat[i][j] === 1 ? 0 : 1
-        } else if(mode === 'location' && mat[i][j] === 1 && (location[0] !== i || location[1] !== j)) {
-          location[0] = i, location[1] = j
-        } else if(mode === 'target' && mat[i][j] === 1 && ((target[0] !== i || target[1] !== j))) {
-          target = [i, j]
+          blocks[i][j] = !blocks[i][j]
+          document.getElementById(`c-${i}-${j}`)!.style.backgroundColor =
+            blocks[i][j] ? 'black' : 'white'
         }
 
-        render()
+        if (
+          mode === 'location' &&
+          !blocks[i][j] &&
+          location != new Pair(i, j)
+        ) {
+          location.first = i
+          location.second = j
+          render()
+        }
+
+        if (
+          mode === 'target' &&
+          !blocks[i][j] &&
+          (target.first !== i || target.second !== j)
+        ) {
+          target.first = i
+          target.second = j
+          render()
+        }
       })
     })
   }
 
   select!.addEventListener('change', () => {
-    mode = (<HTMLSelectElement>document.getElementById('mode')).value as 'blocks' | 'target' | 'location'
+    mode = (<HTMLSelectElement>document.getElementById('mode')).value as
+      | 'blocks'
+      | 'target'
+      | 'location'
   })
 
-  function isSafe(x: number, y: number): boolean {
-    return x < SIZE && y < SIZE && x >= 0 && y >= 0 && mat[x][y] === 1 && !visited[x][y]
-  }
+  startBtn?.addEventListener('click', async () => {
+    render()
 
-  let lo = 0
+    const sol = await getShortestPath(
+      new Pair(location.first, location.second),
+      new Pair(target.first, target.second),
+      blocks
+    )
 
-
-
-  function itr(i: number = 0, j: number = 0, minDist: number = Number.MAX_SAFE_INTEGER, dist: number = 0): number {
-
-    console.log(lo++, "Fetching", i, j, dist)
-
-    if(target[0] === i && target[1] === j) {
-      minDist = Math.min(dist, minDist)      
-
-      const clone = JSON.parse(JSON.stringify(v))
-
-      sols.push(clone)
-      return minDist
-    }
-
-    visited[i][j] = true
-
-    // down
-    if(isSafe(i+1, j)) {
-      v.push([i+1, j])
-      minDist = itr(i+1, j, minDist, dist+1)
-      v.pop()
-    }
-
-    // up
-    if(isSafe(i-1, j)) {
-      v.push([i-1, j])
-      minDist = itr(i-1, j, minDist, dist+1)
-      v.pop()
-    }
-
-    // right
-    if(isSafe(i, j+1)) {
-      v.push([i, j+1])
-      minDist = itr(i, j+1, minDist, dist+1)
-      v.pop()
-    }
-
-    // left
-    if(isSafe(i, j-1)) {
-      v.push([i, j-1])
-      minDist = itr(i, j-1, minDist, dist+1)
-      v.pop()
-    }
-    
-    visited[i][j] = false
-
-    return minDist
-  }
-
-  startBtn?.addEventListener('click', () => {
-    lo = 0
-
-    sols.length = 0
-
-    let mn = itr(location[0], location[1])
-    if(mn !== Number.MAX_SAFE_INTEGER) {
-      const indecies = sols.map(arr => arr.length)
-      const mnLength = Math.min(...indecies)
-      const index = indecies.indexOf(mnLength)
-  
-      for(let i = 0; i < mnLength; i++) {
-        if (sols[index][i][0] != target[0] ||  sols[index][i][1] != target[1])
-        document.getElementById('c-' + sols[index][i][0] + '-' + sols[index][i][1])!.style.backgroundColor = 'red'
-      }
+    if (sol) {
+      sol.forEach((pair) => {
+        if (
+          (pair.first != target.first || pair.second != target.second) &&
+          (pair.first != location.first || pair.second != location.second)
+        )
+          document.getElementById(
+            `c-${pair.first}-${pair.second}`
+          )!.style.backgroundColor = 'red'
+      })
+    } else {
+      alert('No solutions found')
     }
   })
 
